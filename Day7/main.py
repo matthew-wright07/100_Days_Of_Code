d4a88eb12884b12eb4522242787fcb3108aae7fd
Matthew <matthewjwright07@gmail.com>
2025-03-02 20:43:44 -0600

    Made lives and such

--- Day7/main.py
+++ Day7/main.py
@@ -1,18 +1,32 @@
 import random
 
 word_list = ["arrdvark","baboon","camel"]
-
 random_word = random.choice(word_list)
 
-user_letter = input("Guess a letter\n").lower()
+guesses = []
+lives = 5
+game_over = False
 
-response = ""
+while not game_over:
 
-for letter in random_word:
-    if user_letter == letter:
-        response+=user_letter
-    else:
-        response+="_"
+    user_letter = input("Guess a letter\n").lower()
 
-print(response)
-print("This testing")
+    guesses.append(user_letter)
+
+    if user_letter not in random_word:
+            lives -= 1
+
+    response = ""
+
+    for letter in random_word:
+        if letter in guesses:
+            response+=letter
+        else:
+            response+="_"
+    if "_" not in response:
+        game_over=True
+    if not lives>0:
+        game_over=True
+
+    print(response)
+print("Game Over")
