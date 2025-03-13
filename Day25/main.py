8eab79c0d0d4423c7d903417548fe3b99ed966b2
Matthew <matthewjwright07@gmail.com>
2025-03-13 11:47:07 -0500

    More stuff

--- Day25/main.py
+++ Day25/main.py
@@ -1,6 +1,20 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+readable = pandas.read_csv("squirrel_count.csv")
 
-monday = data[data.day == "Monday"]
-print(monday.temp*1.8+32)
+total_cinnamon = len(readable[readable["Primary Fur Color"] == "Cinnamon"])
+total_gray = len(readable[readable["Primary Fur Color"] == "Gray"])
+total_black = len(readable[readable["Primary Fur Color"] == "Black"])
+
+print(total_cinnamon)
+print(total_gray)
+print(total_black)
+
+data = {
+    "Fur Color" : ["Cinnamon", "Gray", "Black"],
+    "Count" : [total_cinnamon,total_gray,total_black]
+}
+
+dataFrame = pandas.DataFrame(data)
+
+dataFrame.to_csv("squirrel_color_count.csv")
